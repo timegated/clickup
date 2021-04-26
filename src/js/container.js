b2ab9bd1f8fe4d37c27f2c2e1c3c3147c6d0d932
dbeccaria826 <beccdaniel@gmail.com>
2021-04-25 20:07:42 -0700

    ClipPath: trying out clip path for trapezoid feature cards

--- src/js/container.js
+++ src/js/container.js
@@ -1,10 +1,11 @@
-import image5 from '../images/image5.png';
 import image3 from '../images/image3.png';
-import image2 from '../images/image2.png';
-import steve from '../images/shteve.PNG';
-import alGore from '../images/al-gore.png';
-import batman from '../images/batman-img.png';
-import user from '../images/random-icon.png';
+import image4 from '../images/image4.png';
+import image5 from '../images/image5.png';
+import image6 from '../images/image6.png';
+import user1 from '../images/user1.jpg';
+import user2 from '../images/user2.jpg';
+import user3 from '../images/user3.jpg';
+import user4 from '../images/user4.jpg';
 
 const container = {
   targets: {
@@ -19,56 +20,56 @@ const container = {
     {
       title: 'Search Data',
       img: image3,
-      copy: 'Don\'t worry if your data is very large, the Data Warehouse provides a search engine, whic is useful for making it easier to find data effectively saving time',
+      copy: 'Don\'t worry if your data is very large, the Data Warehouse provides a search engine, whic is useful for making it easier to find data effectively saving time.',
       btnTxt: 'Learn More'
     },
     {
       title: '24 Hours Access',
-      img: image2,
+      img: image4,
       copy: 'Access is given 24 hours a full morning to night and meet again in the morning, giving you comfort when you need data when urgent.',
       btnTxt: 'Learn More'
     },
     {
       title: 'Print Out',
-      img: image2,
+      img: image5,
       copy: 'Print out service gives you convenience if someday you need print data, just edit it all and just print it.',
       btnTxt: 'Learn More'
     },
     {
       title: 'Security Code',
-      img: image5,
-      copy: 'Data Security is one of our best facilities. Allows for your files to be safer. The file can be secured with a code or password that you created, so onl you can open the file',
+      img: image6,
+      copy: 'Data Security is one of our best facilities. Allows for your files to be safer. The file can be secured with a code or password that you created, so only you can open the file.',
       btnTxt: 'Learn More'
     },
   ],
   slides: [
     {
       name: 'John Fang',
-      img: user,
+      img: user1,
       website: 'wordfaang.com',
       copy: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
     },
     {
-      name: 'Neil Young',
-      img: steve,
+      name: 'Byron Johnson',
+      img: user2,
       website: 'keeponrockininthefreeworld.com',
       copy: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
     },
     {
-      name: 'Batman',
-      img: batman,
+      name: 'Natalie Young',
+      img: user3,
       website: 'whereisrachel.com',
       copy: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
     },
     {
-      name: 'Al Gore',
-      img: alGore,
+      name: 'John Smith',
+      img: user4,
       website: 'manbearpigisreal.com',
       copy: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
     },
     {
-      name: 'Al Gore',
-      img: alGore,
+      name: 'John Smith',
+      img: user4,
       website: 'manbearpigisreal.com',
       copy: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
     },
--- src/js/index.js
+++ src/js/index.js
@@ -5,13 +5,19 @@ import { slider } from './slider';
 import container from './container';
 import heroImg from '../images/image1.png';
 import cardImg from '../images/image2.png';
-import featureImg from '../images/image3.png';
 import '../css/main.scss';
 
+// targets are dom elements, features and slides are arrays
+// used to generate dom elements
 const { targets, features, slides } = container;
+// hero images
 imageHandler(targets.heroImgContainer, heroImg, 500, 250);
 imageHandler(targets.heroCardImg, cardImg, 250, 250);
+
+// feature grid
 featureHandler(targets.featureGrid, features);
+
+// slides
 slidesHandler(targets.sliderContainer, slides);
 
 const { nextSlide, prevSlide } = slider();
