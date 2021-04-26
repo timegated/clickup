ae8f3566378fae658027f77fb09bb988ee560886
dbeccaria826 <beccdaniel@gmail.com>
2021-04-26 10:03:56 -0700

    Added background images to feature cards

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,9 @@ import heroImg from '../images/image1.png';
 import cardImg from '../images/image2.png';
 import logo1 from '../images/logo1.png';
 import logo2 from '../images/logo2.png';
+import blob1 from '../images/blob1.png';
+import blob2 from '../images/blob2.png';
+import blob3 from '../images/blob3.png';
 import '../css/main.scss';
 
 // targets are dom elements, features and slides are arrays
@@ -24,8 +27,25 @@ imageHandler(targets.footerImageTwo, logo2, 20, 35);
 featureHandler(targets.featureGrid, features);
 
 // feature backgrounds
+const featureCards = document.querySelectorAll('.feature__grid > section > .content');
 
+featureCards[1].style = `
+  background-image:url(${blob1});
+  background-position: 70% 100%;
+  background-repeat: no-repeat;
+`;
 
+featureCards[2].style = `
+  background-image:url(${blob3});
+  background-position: 80% 20%;
+  background-repeat: no-repeat;
+`;
+
+featureCards[3].style = `
+  background-image:url(${blob2});
+  background-position: 30% 30%;
+  background-repeat: no-repeat;
+`;
 // slides
 slidesHandler(targets.sliderContainer, slides);
 
